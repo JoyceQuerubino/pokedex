c03b46e8ed9697fd9deceda980bda3eecbb310a9
JoyceQuerubino <joycequerubino5@gmail.com>
2024-08-05 23:05:42 -0300

    fix: finis screen infodetails

--- src/screens/InfoDetails/index.tsx
+++ src/screens/InfoDetails/index.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import { useRoute } from "@react-navigation/native";
-
-import Header from "@components/Header";
 import { ResumePokemonFormatted } from "@services/pokemon/types.pokemon";
-import { Container, Content, ImageContainer, ImageItem, Floor, Title } from "./styles";
 import { capitalizeFirstLetter } from "@helpers/capitalizedFristLetter";
+import Header from "@components/Header";
+import Tag from "@components/Tag";
+import {
+  Container,
+  Content,
+  ImageContainer,
+  ImageItem,
+  Floor,
+  Title,
+  TagsContainers,
+  DescriptionContainer,
+  SubTitle,
+  InfoContainer,
+  InfoTitle,
+  InfoText,
+  InforListTexts,
+  DescriptionContent,
+} from "./styles";
 
 interface InfoDetailsProps {}
 
@@ -12,21 +27,69 @@ export default function InfoDetails({}: InfoDetailsProps) {
   const { params } = useRoute();
   const { pokemon } = params as { pokemon: ResumePokemonFormatted };
 
+  console.log("AQUI 12", pokemon);
+
   const formattedName = capitalizeFirstLetter(pokemon.name);
+  const pokeTypes = pokemon.details.types;
+
+  const handleCapture = () => {
+    console.log("AAA");
+  };
 
   return (
     <Container>
-      <Header onPress={() => console.log("AAA")} />
+      <Header onPress={handleCapture} />
       <Content color={pokemon.info.color}>
-
         <Title>{formattedName}</Title>
+        <TagsContainers>
+          {pokeTypes.length > 0 &&
+            pokeTypes.map((type) => {
+              return <Tag key={type.slot} text={type.type.name} />;
+            })}
+        </TagsContainers>
 
         <ImageContainer>
-          <ImageItem resizeMode="contain" source={{ uri: pokemon.details.sprite }} />
+          <ImageItem
+            resizeMode="contain"
+            source={{ uri: pokemon.details.sprite }}
+          />
           <Floor />
         </ImageContainer>
+
+        <DescriptionContainer>
+          <DescriptionContent>
+            <SubTitle>About</SubTitle>
+
+            <InfoContainer>
+              <InfoTitle>Capture Ratio:</InfoTitle>
+              <InfoText>{pokemon.info.capture_rate}</InfoText>
+            </InfoContainer>
+
+            <InfoContainer>
+              <InfoTitle>Species:</InfoTitle>
+              <InfoText>{pokemon.details.species.name}</InfoText>
+            </InfoContainer>
+
+            <InforListTexts>
+              <InfoTitle>Eggs:</InfoTitle>
+
+              {pokemon.info.egg_groups.map((eggs) => (
+                <InfoText key={eggs.name}>{`- ${eggs.name}`}</InfoText>
+              ))}
+            </InforListTexts>
+
+            <InforListTexts>
+              <InfoTitle>Abilities:</InfoTitle>
+
+              {pokemon.details.abilities.map((ability) => (
+                <InfoText
+                  key={ability.ability.name}
+                >{`- ${ability.ability.name}`}</InfoText>
+              ))}
+            </InforListTexts>
+          </DescriptionContent>
+        </DescriptionContainer>
       </Content>
-      {/* <Title>Pokédex</Title> */}
     </Container>
   );
 }
--- src/screens/InfoDetails/styles.ts
+++ src/screens/InfoDetails/styles.ts
@@ -6,6 +6,10 @@ import { SafeAreaView } from "react-native-safe-area-context";
 const WIDTH = Dimensions.get("window").width;
 const IMAGE_HEIGHT = WIDTH * 0.48;
 
+export type ContentColor = {
+  color?: string;
+};
+
 export const Container = styled(SafeAreaView)`
   flex: 1;
   align-items: center;
@@ -22,21 +26,20 @@ export const Title = styled.Text`
 
   position: absolute;
   top: 16%;
+  left: 24px;
 `;
 
-export const Content = styled.View`
+export const Content = styled.View<ContentColor>`
   margin-top: 16px;
   width: 100%;
-  height: 42%;
-  justify-content: center;
-  align-items: center;
+  height: 38%;
   background-color: ${({ color }) => color && getColorHex(color)};
   border-bottom-right-radius: 36px;
   border-bottom-left-radius: 36px;
 `;
 
 export const ImageContainer = styled.View`
- align-items: center;
+  align-items: center;
   width: 100%;
   height: 100%;
   border-bottom-right-radius: 36px;
@@ -57,5 +60,64 @@ export const ImageItem = styled.Image`
   height: ${IMAGE_HEIGHT}px;
   position: absolute;
   top: 30%;
+  left: 24%;
   z-index: 999;
 `;
+
+export const TagsContainers = styled.View`
+  position: absolute;
+  flex-direction: row;
+  top: 32%;
+  left: 24px;
+`;
+
+export const DescriptionContainer = styled.View`
+  width: 100%;
+  padding: 12px;
+`;
+
+export const DescriptionContent = styled.View`
+  width: 100%;
+  padding: 12px;
+  border-radius: 16px;
+  margin-top: 16px;
+  background-color: #434343
+`;
+
+export const SubTitle = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.XL}px;
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    color: ${theme.COLORS.LIGHT};
+  `}
+  margin-bottom: 16px;
+`;
+
+export const InfoContainer = styled.View`
+  flex-direction: row;
+  margin-bottom: 16px;
+`;
+
+
+export const InfoTitle = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.LG}px;
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    color: ${theme.COLORS.LIGHT};
+  `}
+`;
+
+
+export const InfoText = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.LG}px;
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    color: ${theme.COLORS.LIGHT};
+  `}
+  margin-left: 8px;
+`;
+
+export const InforListTexts = styled.View`
+  margin-bottom: 16px;
+`;
+
